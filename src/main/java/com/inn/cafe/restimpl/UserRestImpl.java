ee18331a9ffd98f66ab4bee5316175c125dccaf5
Md.Mehedi Hasan <ght.32rd@gmail.com>
2025-01-28 15:52:59 +0600

    yest running

--- src/main/java/com/inn/cafe/restimpl/UserRestImpl.java
+++ src/main/java/com/inn/cafe/restimpl/UserRestImpl.java
@@ -1,6 +1,10 @@
 package com.inn.cafe.restimpl;
 
 import com.inn.cafe.rest.UserRest;
+import com.inn.cafe.service.UserService;
+import com.inn.cafe.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +12,16 @@ import java.util.Map;
 
 @RestController
 public class UserRestImpl implements UserRest {
+    @Autowired
+    UserService userService;
     @Override
     public ResponseEntity<String> signup(Map<String, String> requestMap) {
         try {
+            return userService.signup(requestMap);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
+        return CafeUtils.getResponseEntity("",HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
